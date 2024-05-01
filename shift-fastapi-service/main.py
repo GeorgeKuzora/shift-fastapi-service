58a2759d7f7d2f70557ea7ab82e51dcf49a46ba8
GeorgiyKuzora <rafale87@gmail.com>
2024-05-01 08:02:34 +0300

    Add get_salary_by_user_id view

--- shift-fastapi-service/main.py
+++ shift-fastapi-service/main.py
@@ -1,6 +1,8 @@
 import logging
 
+from exceptions import DataNotFoundException
 from fastapi import FastAPI, status
+from repository import get_user_by_id
 
 logging.basicConfig(filename="logs/main.log", level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -16,3 +18,16 @@ def not_found() -> dict[str, str]:
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/salary/{user_id}")
+def get_salary_by_user_id(
+    user_id: int,
+) -> dict[str, str] | dict[str, str | int | date]:
+    try:
+        user = get_user_by_id(user_id)
+    except DataNotFoundException:
+        logger.info(f"not found user with id: {user_id}", exc_info=True)
+        return not_found()
+    response = {"user": user.get("name", ""), "salary": user.get("salary", 0)}
+    return response
