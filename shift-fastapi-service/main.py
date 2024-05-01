25ab388dba72c39de9f3c5ff28c2932e3465c32f
GeorgiyKuzora <rafale87@gmail.com>
2024-05-01 08:01:26 +0300

    Add 404 response method

--- shift-fastapi-service/main.py
+++ shift-fastapi-service/main.py
@@ -1,11 +1,16 @@
 import logging
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 logging.basicConfig(filename="logs/main.log", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+
+@app.get("/404", status_code=status.HTTP_404_NOT_FOUND)
+def not_found() -> dict[str, str]:
+    return {"message": "Resource Not Found"}
 
 
 @app.get("/")
