1918c2113a71396ec28f3344978f44ef22739b5c
GeorgiyKuzora <rafale87@gmail.com>
2024-05-01 10:03:05 +0300

    Add 404 status handler

--- shift-fastapi-service/main.py
+++ shift-fastapi-service/main.py
@@ -2,7 +2,8 @@ import logging
 from datetime import date
 
 from exceptions import DataNotFoundException
-from fastapi import FastAPI, status
+from fastapi import FastAPI, Request, Response, status
+from fastapi.responses import RedirectResponse
 from logs import init_logging
 from repository import get_user_by_id
 
@@ -10,6 +11,11 @@ init_logging()
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+
+@app.exception_handler(404)
+def not_found_handler(request: Request, exc: Exception):
+    return RedirectResponse(url="/404")
 
 
 @app.get("/404", status_code=status.HTTP_404_NOT_FOUND)
