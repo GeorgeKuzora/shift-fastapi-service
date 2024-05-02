f807f06ea6118963c457246b3f48e4ba11506f6d
GeorgiyKuzora <rafale87@gmail.com>
2024-05-02 21:42:32 +0300

    Set main.py as entrypoint

--- shift-fastapi-service/app.py
+++ shift-fastapi-service/app.py
@@ -1,6 +1,3 @@
 from fastapi import FastAPI
-from logs import init_logging
-
-init_logging()
 
 app = FastAPI()
---
+++ shift-fastapi-service/main.py
@@ -0,0 +1,7 @@
+import app
+import views
+from dotenv import load_dotenv
+from logs import init_logging
+
+load_dotenv()
+init_logging()
