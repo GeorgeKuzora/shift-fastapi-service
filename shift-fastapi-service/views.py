420d981da0b5c6bcbf82648828e241a9cfec8b83
GeorgiyKuzora <rafale87@gmail.com>
2024-05-04 12:00:25 +0300

    Add get me views

--- shift-fastapi-service/views.py
+++ shift-fastapi-service/views.py
@@ -1,11 +1,13 @@
 import logging
-from datetime import date
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, Request, status
+from fastapi.responses import RedirectResponse
 
 from app import app
+from auth.auth import get_current_active_user
+from domain import User, UserNextPromotionDate, UserSalary
 from exceptions import DataNotFoundException
-from fastapi import HTTPException, Request, Response, status
-from fastapi.responses import RedirectResponse
-from repository import get_user_by_id
 
 logger = logging.getLogger(__name__)
 
@@ -20,5 +22,26 @@ async def not_found() -> dict[str, str]:
     return {"message": "Resource Not Found"}
 
 
+@app.get("/user/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+) -> User:
+    return current_user
 
 
+@app.get("/salary/me", response_model=UserSalary)
+async def get_salary_for_current_user(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+) -> UserSalary:
+    user_salary: UserSalary = current_user.get_salary()
+    return user_salary
+
+
+@app.get("/promotion/me", response_model=UserNextPromotionDate)
+async def get_next_promotion_date_for_current_user(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+) -> UserNextPromotionDate:
+    user_next_promotion_date: UserNextPromotionDate = (
+        current_user.get_next_promotion_date()
+    )
+    return user_next_promotion_date
