331b810a29230fcb5536c93f39b614a1410ddb1b
GeorgiyKuzora <rafale87@gmail.com>
2024-05-05 12:11:26 +0300

    Refactor init_logging function

--- shift_fastapi_service/logs.py
+++ shift_fastapi_service/logs.py
@@ -8,16 +8,17 @@ LOG_DIR_NAME = "logs"
 MAIN_LOG_NAME = "main.log"
 
 
-def init_logging() -> None:
-    p = Path(".")
-    log_dir_path = p / LOG_DIR_NAME
-    if not log_dir_path.exists():
-        create_log_directory(log_dir_path)
-    elif not log_dir_path.is_dir():
+def init_logging(log_root: Path = Path(".")) -> None:
+    log_dir_path = log_root / LOG_DIR_NAME
+    if log_dir_path.exists() and not log_dir_path.is_dir():
         print(
             f"{log_dir_path.absolute()} already exists and isn't a directory"
         )
-        raise LoggingConfigException
+        raise LoggingConfigException(
+            f"{log_dir_path.absolute()} already exists and isn't a directory"
+        )
+    elif not log_dir_path.exists():
+        create_log_directory(log_dir_path)
     main_log_path = log_dir_path / MAIN_LOG_NAME
     logging.basicConfig(filename=main_log_path, level=logging.INFO)
 
