8a614430dd0b0d4a81d3ef43bf76e565c8d5249f
GeorgiyKuzora <rafale87@gmail.com>
2024-05-06 21:37:17 +0300

    Add create_user to Repository class

--- shift_fastapi_service/repository.py
+++ shift_fastapi_service/repository.py
@@ -3,11 +3,13 @@ from datetime import date
 
 from passlib.context import CryptContext
 from sqlalchemy import create_engine, select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from shift_fastapi_service.exceptions import (
     DatabaseException,
     DataNotFoundException,
+    NotUniqueException,
 )
 from shift_fastapi_service.models import Base, User
 
@@ -62,6 +64,26 @@ class Repository:
                 logger.info(f"not found user with username: {username}")
                 raise DataNotFoundException
             return user.to_dict()
+
+    def create_user(self, user: dict):
+        with Session(engine) as session:
+            user_in_db = User(
+                username=user.get("username"),
+                email=user.get("email"),
+                salary=user.get("salary"),
+                next_promotion_date=user.get("next_promotion_date"),
+                disabled=user.get("disabled"),
+                hashed_password=user.get("hashed_password"),
+            )
+            try:
+                session.add(user_in_db)
+                session.flush()
+                session.commit()
+            except IntegrityError:
+                logger.info(
+                    f"user {user_in_db.username} or user with email {user_in_db.email} already exists"
+                )
+                raise NotUniqueException
 
 
 if __name__ == "__main__":
